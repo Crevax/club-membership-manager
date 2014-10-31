f2101fb820eedc29b0f366a976a29cd24c027a21
CJ Davis <Christopher.Davis@cqlcorp.com>
2014-10-31 15:05:48 -0400

    Add basic start for Clubs resource

---
+++ ClubMembershipManager.Tests/Controllers/ClubsControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClubMembershipManager;
+using ClubMembershipManager.Controllers;
+using ClubMembershipManager.Models;
+
+namespace ClubMembershipManager.Tests.Controllers
+{
+	[TestClass]
+	public class ClubsControllerTest
+	{
+		[TestMethod]
+		public void Get()
+		{
+			// Arrange
+			ClubsController controller = new ClubsController();
+
+			// Act 
+			IEnumerable<Club> result = controller.Get();
+
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(2, result.Count());
+			Assert.AreEqual("Grand Rapids Steelheaders", result.ElementAt(0).Name);
+			Assert.AreEqual("Circle K at Davenport University", result.ElementAt(1).Name);
+		}
+	}
+}
---
+++ ClubMembershipManager/Controllers/ClubsController.cs
@@ -0,0 +1,31 @@
+﻿using ClubMembershipManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClubMembershipManager.Controllers
+{
+    public class ClubsController : ApiController
+    {
+        // GET api/clubs
+        public IEnumerable<Club> Get()
+        {
+            return new Club[]
+            { 
+                new Club
+                {
+                    Id = 1,
+                    Name = "Grand Rapids Steelheaders"
+                },
+                new Club
+                {
+                    Id = 2,
+                    Name = "Circle K at Davenport University"
+                }
+            };
+        }
+    }
+}
