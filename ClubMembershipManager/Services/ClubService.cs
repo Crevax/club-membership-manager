d0b699ccd56e008545df115fd203e40f4dc2dcd8
CJ Davis <Christopher.Davis@cqlcorp.com>
2014-11-14 16:19:15 -0500

    Readd services and implement an interface

---
+++ ClubMembershipManager/Services/ClubService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClubMembershipManager.Services
+{
+	public class ClubService : IClubService
+	{
+	}
+}
---
+++ ClubMembershipManager/Services/IClubService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ClubMembershipManager.Models;
+
+namespace ClubMembershipManager.Services
+{
+	public interface IClubService
+	{
+		IList<Club> GetAllClubs();
+		Club GetClubByID(int Id);
+	}
+}
