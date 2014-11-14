d30bf1aecdeb1b32c6f3df993305b5a46b5ae373
CJ Davis <Christopher.Davis@cqlcorp.com>
2014-11-14 15:17:00 -0500

    Change Club Repository to use PetaPOCO

--- ClubMembershipManager/Services/ClubRepository.cs
+++ ClubMembershipManager/Services/ClubRepository.cs
@@ -6,6 +6,7 @@ using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using PetaPoco;
 
 namespace ClubMembershipManager.Services
 {
@@ -13,29 +14,9 @@ namespace ClubMembershipManager.Services
     {
         public List<Club> GetAllClubs()
         {
-			List<Club> clubs = new List<Club>();
+			Database Memberships = new Database("ClubMembershipManager");
 
-			var connectionString = ConfigurationManager.ConnectionStrings["ClubMembershipManager"].ConnectionString;
-			var connection = new SqlConnection(connectionString);
-            connection.Open();
-
-			using (var command = connection.CreateCommand())
-			{
-				command.CommandText = "SELECT * FROM Clubs";
-
-				using (var reader = command.ExecuteReader())
-				{
-					while (reader.Read())
-					{
-						Club club = new Club();
-						club.Id = (int)reader["ID"];
-						club.Name = (string)reader["Name"];
-						clubs.Add(club);
-					}
-				}
-			}
-
-			return clubs;
+			return Memberships.Query<Club>("SELECT * FROM Clubs").ToList<Club>();
         }
     }
 }
